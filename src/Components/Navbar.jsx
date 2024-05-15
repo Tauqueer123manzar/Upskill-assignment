853bc65de40ebca38d1046f2a34b87d630107fd4
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-05-16 01:24:49 +0530

    First commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='Header'>
+      <h1>LOGO</h1>
+      <h2>Login</h2>
+      <button type='button'>Signup</button>
+    </div>
+  )
+}
+export default Navbar
---
+++ src/Components/Topbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Topbar.css'
+const Topbar = () => {
+  return (
+    <div className='Top'>
+      <ul>
+       <li>
+         <a href='#overview'>Overview</a>
+       </li>
+       <li>
+         <a href='#currculum'>Curriculum</a>
+       </li>
+       <li>
+         <a href='#Refund'>Refund</a>
+       </li>
+       <li>
+         <a href='#Testinomials'>Testinomails</a>
+       </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Topbar
